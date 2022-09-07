483fac97a9141083a7f024fb29f14ce5052e2928
Igor Mazur <62945459+Grungeshum@users.noreply.github.com>
2022-09-07 17:16:48 +0300

    fix fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,17 @@
-export default function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-
-  return fetch(url).then(responce => responce.json());
-}
+export const fetchCountries = name => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        if (response.status === 404) {
+          return [];
+        }
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
